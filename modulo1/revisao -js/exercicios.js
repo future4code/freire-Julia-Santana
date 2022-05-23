bdcdc015c7d9a13f87d0ac878eccc62d82df3c21
Julia Moreira de Santana <juliamorsantana@gmail.com>
2022-05-23 14:45:51 -0300

    exercicio 1 ao 4

---
+++ modulo1/revisao -js/exercicios.js
@@ -0,0 +1,99 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray() {
+   const lista = [1,2,3,4,5,6]
+   return lista.length
+   
+}
+console.log(retornaTamanhoArray());
+
+// EXERCÍCIO 02
+function retornaArrayInvertido() {
+   const lista = [1, 2, 3, 4, 5]
+   return lista.reverse()
+}
+console.log(retornaArrayInvertido());
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array = [50, 40, 30, 20, 10]) {
+      return array.sort()
+   }
+   console.log(retornaArrayOrdenado());
+
+
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array = [5, 8, 4, 2, 9, 6]) {
+         const numerosPares = array / 2 === 0
+         return numerosPares
+}
+console.log(retornaNumerosPares());
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
