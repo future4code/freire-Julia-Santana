7a73d26e1284a59e3d754dac2ccf5dd0146d130a
Julia Moreira de Santana <juliamorsantana@gmail.com>
2022-07-15 16:47:55 -0300

    atualizações

--- modulo2/projeto-labex/projeto-labex/src/Constantes/url.js
+++
@@ -1,1 +0,0 @@
-export const urlViagens =  "https://us-central1-labenu-apis.cloudfunctions.net/labeX/Julia-Moreira-Freire/trips"
---
+++ modulo2/projeto-labex/projeto-labex/src/pages/ApplicationFormPage/ApplicationFormPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import  imgApplicationFormPage from "../../assets/ApplicationFormPage.jpg";
+import {goToHomePage, goToVoltar} from '../../routes/coordinator';
+import {ApplytoTrip} from '../../hooks/Axios';
+
+const DivApplicationFormPage = styled.div`
+
+.div-img-tela{
+  background-image: url(${imgApplicationFormPage});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: auto;
+  height: 100vh;
+}
+
+`
+
+const ApplicationFormPage = () => {
+
+  const navigate = useNavigate()
+  
+  const id = "OB43AXb1cSQ9sjSpVZ4S"
+
+  return (
+        <DivApplicationFormPage> 
+          <div className='div-img-tela'>
+
+            <h1>  ApplicationFormPage</h1>
+            <button onClick={() => goToHomePage(navigate)}> HomePage </button>
+            <button onClick={() => goToVoltar(navigate)}> voltar </button>
+            <button onClick={() => {alert("Formulario Enviado")}}> Enviar </button>
+            <button onClick={() => {ApplytoTrip(id)}}> Ver se o formulario ta enviando </button>
+          </div>
+
+        </DivApplicationFormPage>
+  );
+}
+
+
+export default ApplicationFormPage;
