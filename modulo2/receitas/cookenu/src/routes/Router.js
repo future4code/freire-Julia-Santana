50dd6720e9506f1a3d52d3c6b559d96cdcb538d0
Julia Moreira de Santana <juliamorsantana@gmail.com>
2022-07-25 18:25:31 -0300

    criaando paginas

---
+++ modulo2/receitas/cookenu/src/routes/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddRecipePage from "../pages/AddRecipesPage/AddRecipesPage"
+import LoginPage from '../pages/LoginPage/LoginPage'
+import RecipeDetailPage from '../pages/RecipeDetailPage/RecipeDetailPage'
+import RecipesListPage from '../pages/RecipesListPage/RecipeListPage'
+import SingUpPage from '../pages/SingUpPage/SingUpPage'
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+                <Route path={"login"} element={<LoginPage />} />
+
+                <Route path={"/cadastro"} element={<SingUpPage />} />
+
+                <Route path={"/"} element={<RecipesListPage />} />
+
+                <Route path={"/adicionar-receita"} element={<AddRecipePage />} />
+
+                <Route path={"/detalhe/:id"} element={<RecipeDetailPage />} />
+
+                <Route path={"/erro"} element={<ErrorPage />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router
