cd60e0d17199095f850449a73028f734769951be
JulSantana <juliamorsantana@gmail.com>
2022-08-30 18:36:01 -0300

    Exercicio-filtro-ordenacao-paginacao

---
+++ modulo4/filtros-ordenacao-paginacao/src/exercicio1.ts
@@ -0,0 +1,40 @@
+import { connection } from "./data/connection";
+import { Request, Response } from "express";
+
+//  Crie uma cópia do endpoint acima, e altere-o para que ele possa 
+//receber um parâmetro de filtragem por nome. Este nome deve ser enviado por query params.
+
+
+export async function selectAllUsers(): Promise<any> {
+   const result = await connection.raw(`
+       SELECT id, name, email, type
+       FROM aula48_exercicio;
+    `)
+
+   return result[0]
+}
+
+export async function selectUsersByName(name:string): Promise<any> {
+
+      const result = await connection("aula48_exercicio")
+         .where("name", "LIKE", `%${name}%`)
+         return result
+
+}
+
+export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
+   try {
+      const users = await selectAllUsers()
+
+      if (!users.length) {
+         throw new Error("No recipes found")
+      }
+
+      res.status(200).send(users)
+
+   } catch (error: any) {
+      const resp = error.response.data || error.message
+      console.log(resp);
+ 
+       }
+}
---
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import { AddressInfo } from "net";
+import { selectUsersByName } from "./exercicio1";
+import { getUsersByName } from "./endpoints/getUsersByName";
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/users", getUsersByName)
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
