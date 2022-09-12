9ace95183718c429f90f8baa7f6b43d26f63d6f3
JulSantana <juliamorsantana@gmail.com>
2022-09-12 16:57:00 -0300

    exercicios introducao labesystem

---
+++ modulo4/introducao-autenticacao/src/services/exercicio3.ts
@@ -0,0 +1,19 @@
+
+import * as jwt from "jsonwebtoken";
+
+const expiresIn = "1min"
+
+const generateToken = (id: string): string => {
+  const token = jwt.sign(
+    {
+      id
+    },
+    process.env.JWT_KEY as string,
+    {
+      expiresIn
+    }
+  );
+  return token;
+}
+
+//a) O que a linha as string faz? Por que precisamos usar ela ali?
